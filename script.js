b21abd30547ef8e03ef9b4af912ed0d219feabbe
Filipe de Oliveira Ataíde <filipeoliveirataide@gmail.com>
2022-07-01 09:00:25 -0300

    Slider funcionando

---
+++ script.js
@@ -0,0 +1,27 @@
+$(window).scroll(function () {
+    var scroll_position = $(window).scrollTop();
+    $('.sec').each(function () {
+        var sec = $(this);
+        $('.slider-item').each(function () {
+            var sec_name = $(this).data('sec');
+            var position = sec.position().top - 10;
+            if (sec.hasClass(sec_name)) {
+                if (scroll_position >= position) {
+                    $('.slider-item').removeClass('slider-item-active');
+                    $(this).addClass('slider-item-active');
+                }
+            }
+        })
+    })
+})
+
+$('.slider-item').click(function () {
+    var data_sec = $(this).data('sec')
+    $('.sec').each(function () {
+        if($(this).hasClass(data_sec)){
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(this).position().top
+            }, 100);
+        }
+    })
+})
